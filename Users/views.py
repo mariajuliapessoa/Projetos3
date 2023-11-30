2a392c7df67304e627cda61df757de586e1620e2
MarinaFFSC <mffcc@cesar.school>
2023-11-29 22:29:12 -0300

    versao hamburguer

--- Users/views.py
+++ Users/views.py
@@ -1,9 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render 
 from rolepermissions.decorators import has_permission_decorator
+from django.contrib.auth import Users
 
 @has_permission_decorator('cadastrar_Users')
 
 def cadastrar_Users(request):
-	return render(request, 'cadastrar_Users.html')
+	User = Users()
+	if request.method == 'POST':
+		form = CustomUserCreationForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned.data['username']
+			email = form.cleaned.data['email']
+			password1 = form.cleaned.data['password1']
+			password2 = form.cleaned.data['password2']
+			first_name = form.cleaned.data['first_name']
+			last_name = form.cleaned.data['last_name']
+			tipo_usuario = form.cleaned.data['tipo_usuario']
+
+			Users = form.save(commit=False)
+			Users.set_password(form.cleaned_data['password1'])
+			Users.save()
+
+			return redirect('Projetos3/home.html')
+
+	else: 
+		form = CustomUserCreationForm()
+
+	return render(request, 'cadastrar_Users.html', {'form': form})
 
